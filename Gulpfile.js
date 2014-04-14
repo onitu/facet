5a6164e8d62a0c7938eabee212962c584f8d0ce3
Yannick PEROUX <yannick.peroux@gmail.com>
2014-04-15 00:10:40 +0100

    Add basic Gulpfile

---
+++ Gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require("gulp")
+var browserify = require('gulp-browserify')
+var connect = require("gulp-connect")
+var open = require("gulp-open")
+
+gulp.task('scripts', function() {
+  gulp.src(['app/scripts/*.js'])
+    .pipe(browserify())
+    .pipe(gulp.dest('./build'))
+})
+
+gulp.task('stylesheets', function() {
+  gulp.src(['app/stylesheets/*.css'])
+    .pipe(gulp.dest('./build'))
+})
+
+gulp.task('content', function() {
+  gulp.src(['app/**/*.html'])
+    .pipe(gulp.dest('./build/'))
+})
+
+gulp.task('server', function() {
+   connect.server({
+    root : './build',
+    port : 4242,
+    livereload : { port: 4243 }
+  })
+
+  gulp.src('./build/index.html')
+    .pipe(open('', {url: 'http://localhost:4242'}))
+})
+
+gulp.task('reload', function() {
+  connect.reload()
+})
+
+gulp.task('watch', function() {
+  gulp.watch('app/stylesheets/*.css', ['stylesheets'])
+  gulp.watch('app/scripts/*.js', ['scripts'])
+  gulp.watch('app/**/*.html', ['content', 'reload'])
+})
+
+gulp.task('dist', [
+  "scripts",
+  "stylesheets",
+  "content"
+])
+
+gulp.task("default", [
+  "dist",
+  "server",
+  "watch"
+])
