e826820dc7e3257679c00df17148faa46552845b
Yannick PEROUX <yannick.peroux@gmail.com>
2014-04-17 18:27:51 +0100

    Use coffeeify instead of gulp-coffee

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,30 +1,33 @@
 var gulp = require("gulp")
 var util = require("gulp-util")
+var rename = require('gulp-rename')
 var browserify = require("gulp-browserify")
 var livereload = require("gulp-livereload")
 var embedlr = require("gulp-embedlr")
 var sass = require("gulp-sass")
 var jade = require("gulp-jade")
-var coffee = require("gulp-coffee")
 var open = require("gulp-open")
 
 gulp.task('scripts', function() {
-  return gulp.src(['app/scripts/app.coffee'])
-    .pipe(coffee().on('error', util.log))
-    .pipe(browserify().on('error', util.log))
+  return gulp.src('app/scripts/app.coffee', {read: false})
+    .pipe(browserify({
+      transform: ['coffeeify'],
+      extensions: ['.coffee']
+    }).on('error', util.log))
+    .pipe(rename('app.js'))
     .pipe(gulp.dest('./build'))
     .pipe(livereload())
 })
 
 gulp.task('stylesheets', function() {
-  return gulp.src(['app/stylesheets/app.scss'])
+  return gulp.src('app/stylesheets/app.scss')
     .pipe(sass().on('error', util.log))
     .pipe(gulp.dest('./build'))
     .pipe(livereload())
 })
 
 gulp.task('content', function() {
-  return gulp.src(['app/**/*.jade'])
+  return gulp.src('app/**/*.jade')
     .pipe(jade().on('error', util.log))
     .pipe(embedlr())
     .pipe(gulp.dest('./build/'))
