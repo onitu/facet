589e98e85d9c0e2e54f817447ce1cda5d49a9654
Frank LENORMAND <lenormf@gmail.com>
2014-06-08 17:25:32 +0200

    Added a route manager, fixed the view on the main template

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 
 "use strict";
 
+var facetApp = angular.module("facetApp", [ "ngRoute", "facetFilters", "facetControllers" ]);
+
+facetApp.config([ "$routeProvider",
+	function ($rp) {
+		$rp.when("/driver/add", {
+			templateUrl: "partials/add_driver_form.html",
+			// FIXME: controller: "",
+		})
+		$rp.when("/files", {
+			templateUrl: "partials/files_list.html",
+			controller: "filesListCtrl",
+		})
+		.otherwise({
+			redirectTo: "/files",
+		});
+	}
+]);
