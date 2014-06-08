bc228da1766a6b9054d358a466b27928480ed791
Frank LENORMAND <lenormf@gmail.com>
2014-06-08 17:26:58 +0200

    Added a files controller, added a filter for the file size, fixed the partial template for the files listing

--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,25 @@
 
 "use strict";
 
-var facet = angular.module("facetApp", []);
+var facetControllers = angular.module("facetControllers", []);
 
-facet.controller("DriverListCtrl", function ($scope) {
-	// FIXME: get the drivers from the server
-	$scope.drivers = [
-		{"name": "Secure Shell"},
-		{"name": "Dropbox"},
-		{"name": "Local"},
-	];
-});
+facetControllers.controller("driverListCtrl", [ "$scope", "$http",
+	function ($scope, $http) {
+		// FIXME: get the drivers from the server
+		$scope.drivers = [
+			{"name": "Secure Shell"},
+			{"name": "Dropbox"},
+			{"name": "Local"},
+		];
+	}
+]);
+facetControllers.controller("filesListCtrl", [ "$scope", "$http",
+	function ($scope, $http) {
+		// FIXME: get the files from the server
+		$scope.files = [
+			{"filename": "/etc/passwd", "size": "1234", "owners": [ "Dropbox" ]},
+			{"filename": "42.zip", "size": "10101010", "owners": [ "Local" ]},
+			{"filename": "xxx.flv", "size": "0", "owners": [ "Google Drive" ]},
+		];
+	}
+]);
