7d8170e676c95cc2695576b6e24528412931166d
Frank LENORMAND <lenormf@gmail.com>
2014-12-07 11:09:29 +0100

    Fixed the settings to be aware of endpoint URL modifications

--- js/controllers/settings.js
+++ js/controllers/settings.js
@@ -5,7 +5,12 @@
 
 "use strict";
 
-facetControllers.controller("settingsFormCtrl", [ "$scope",
-    function ($scope) {
+facetControllers.controller("settingsFormCtrl", [ "$scope", "Restangular",
+    function ($scope, Restangular) {
+        $scope.endpoint = Restangular.configuration.baseUrl;
+
+        $scope.updateSettings = function () {
+            Restangular.setBaseUrl($scope.endpoint);
+        }
     }
 ]);
